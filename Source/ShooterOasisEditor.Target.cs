9f01a8814dcf8f807d3627185630cf2dc8b26433
PaulB <PaulBerry7538@Gmail.com>
2026-01-10 18:44:23 +0000

    Initial commit

---
+++ Source/ShooterOasisEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ShooterOasisEditorTarget : TargetRules
+{
+	public ShooterOasisEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		ExtraModuleNames.Add("ShooterOasis");
+	}
+}
